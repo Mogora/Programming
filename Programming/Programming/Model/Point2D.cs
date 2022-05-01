474231c021c0bd3af5407d06a2668a9f6a3c1101
Тамила Хасанова <tamosca@mail.ru>
2022-05-01 22:31:30 +0300

    1.05.2022 Add: добавлен класс Point2D

---
+++ Programming/Programming/Model/Point2D.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programming.Model
+{
+    class Point2D
+    {
+        private float _x;
+
+        private float _y;
+
+        public Point2D ()
+        {
+            X = _x;
+            Y = _y;
+        }
+
+        public float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                Validator.AssertOnPositiveValue(nameof(X), value);
+                _x = value;
+            }      
+        }
+
+        public float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                Validator.AssertOnPositiveValue(nameof(Y), value);
+                _y = value;
+            }
+        }
+
+    }
+}
