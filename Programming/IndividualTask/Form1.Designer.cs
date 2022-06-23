0cc7744b6fed8ae58cffe240cc21b825a11e1801
Тамила Хасанова <tamosca@mail.ru>
2022-06-23 16:02:02 +0300

    23.06.2022 Создано новое решение

--- Programming/IndividualTask/Form1.Designer.cs
+++ Programming/IndividualTask/Form1.Designer.cs
@@ -47,11 +47,11 @@ namespace IndividualTask
             // BuildingsListBox
             // 
             this.BuildingsListBox.FormattingEnabled = true;
-            this.BuildingsListBox.ItemHeight = 25;
-            this.BuildingsListBox.Location = new System.Drawing.Point(5, 11);
-            this.BuildingsListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BuildingsListBox.ItemHeight = 18;
+            this.BuildingsListBox.Location = new System.Drawing.Point(4, 8);
+            this.BuildingsListBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.BuildingsListBox.Name = "BuildingsListBox";
-            this.BuildingsListBox.Size = new System.Drawing.Size(336, 579);
+            this.BuildingsListBox.Size = new System.Drawing.Size(253, 418);
             this.BuildingsListBox.TabIndex = 0;
             this.BuildingsListBox.SelectedIndexChanged += new System.EventHandler(this.BuildingsListBox_SelectedIndexChanged);
             // 
@@ -65,11 +65,11 @@ namespace IndividualTask
             this.BuildingsGroupBox.Controls.Add(this.RatingTextBox);
             this.BuildingsGroupBox.Controls.Add(this.AddressTextBox);
             this.BuildingsGroupBox.Controls.Add(this.TitleTextBox);
-            this.BuildingsGroupBox.Location = new System.Drawing.Point(349, 11);
-            this.BuildingsGroupBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BuildingsGroupBox.Location = new System.Drawing.Point(262, 8);
+            this.BuildingsGroupBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.BuildingsGroupBox.Name = "BuildingsGroupBox";
-            this.BuildingsGroupBox.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.BuildingsGroupBox.Size = new System.Drawing.Size(524, 249);
+            this.BuildingsGroupBox.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.BuildingsGroupBox.Size = new System.Drawing.Size(393, 179);
             this.BuildingsGroupBox.TabIndex = 1;
             this.BuildingsGroupBox.TabStop = false;
             this.BuildingsGroupBox.Text = "Selected buildings:";
@@ -77,73 +77,82 @@ namespace IndividualTask
             // CategoryComboBox
             // 
             this.CategoryComboBox.FormattingEnabled = true;
-            this.CategoryComboBox.Location = new System.Drawing.Point(146, 115);
+            this.CategoryComboBox.Location = new System.Drawing.Point(110, 83);
+            this.CategoryComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CategoryComboBox.Name = "CategoryComboBox";
-            this.CategoryComboBox.Size = new System.Drawing.Size(289, 33);
+            this.CategoryComboBox.Size = new System.Drawing.Size(218, 26);
             this.CategoryComboBox.TabIndex = 2;
             // 
             // RatingLabel
             // 
             this.RatingLabel.AutoSize = true;
-            this.RatingLabel.Location = new System.Drawing.Point(27, 159);
+            this.RatingLabel.Location = new System.Drawing.Point(20, 114);
+            this.RatingLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.RatingLabel.Name = "RatingLabel";
-            this.RatingLabel.Size = new System.Drawing.Size(73, 25);
+            this.RatingLabel.Size = new System.Drawing.Size(54, 18);
             this.RatingLabel.TabIndex = 7;
             this.RatingLabel.Text = "Rating:";
             // 
             // CategoryLabel
             // 
             this.CategoryLabel.AutoSize = true;
-            this.CategoryLabel.Location = new System.Drawing.Point(27, 115);
+            this.CategoryLabel.Location = new System.Drawing.Point(20, 83);
+            this.CategoryLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.CategoryLabel.Name = "CategoryLabel";
-            this.CategoryLabel.Size = new System.Drawing.Size(98, 25);
+            this.CategoryLabel.Size = new System.Drawing.Size(72, 18);
             this.CategoryLabel.TabIndex = 6;
             this.CategoryLabel.Text = "Category:";
             // 
             // AddressLabel
             // 
             this.AddressLabel.AutoSize = true;
-            this.AddressLabel.Location = new System.Drawing.Point(27, 76);
+            this.AddressLabel.Location = new System.Drawing.Point(20, 55);
+            this.AddressLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.AddressLabel.Name = "AddressLabel";
-            this.AddressLabel.Size = new System.Drawing.Size(91, 25);
+            this.AddressLabel.Size = new System.Drawing.Size(66, 18);
             this.AddressLabel.TabIndex = 5;
             this.AddressLabel.Text = "Address:";
             // 
             // TitleLabel
             // 
             this.TitleLabel.AutoSize = true;
-            this.TitleLabel.Location = new System.Drawing.Point(27, 35);
+            this.TitleLabel.Location = new System.Drawing.Point(20, 25);
+            this.TitleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TitleLabel.Name = "TitleLabel";
-            this.TitleLabel.Size = new System.Drawing.Size(55, 25);
+            this.TitleLabel.Size = new System.Drawing.Size(39, 18);
             this.TitleLabel.TabIndex = 4;
             this.TitleLabel.Text = "Title:";
             // 
             // RatingTextBox
             // 
-            this.RatingTextBox.Location = new System.Drawing.Point(146, 159);
+            this.RatingTextBox.Location = new System.Drawing.Point(110, 114);
+            this.RatingTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.RatingTextBox.Name = "RatingTextBox";
-            this.RatingTextBox.Size = new System.Drawing.Size(289, 30);
+            this.RatingTextBox.Size = new System.Drawing.Size(218, 24);
             this.RatingTextBox.TabIndex = 3;
             // 
             // AddressTextBox
             // 
-            this.AddressTextBox.Location = new System.Drawing.Point(146, 76);
+            this.AddressTextBox.Location = new System.Drawing.Point(110, 55);
+            this.AddressTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AddressTextBox.Name = "AddressTextBox";
-            this.AddressTextBox.Size = new System.Drawing.Size(289, 30);
+            this.AddressTextBox.Size = new System.Drawing.Size(218, 24);
             this.AddressTextBox.TabIndex = 1;
             // 
             // TitleTextBox
             // 
-            this.TitleTextBox.Location = new System.Drawing.Point(146, 35);
+            this.TitleTextBox.Location = new System.Drawing.Point(110, 25);
+            this.TitleTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.TitleTextBox.Name = "TitleTextBox";
-            this.TitleTextBox.Size = new System.Drawing.Size(289, 30);
+            this.TitleTextBox.Size = new System.Drawing.Size(218, 24);
             this.TitleTextBox.TabIndex = 0;
             // 
             // AddBuildingButton
             // 
-            this.AddBuildingButton.Location = new System.Drawing.Point(408, 278);
+            this.AddBuildingButton.Location = new System.Drawing.Point(306, 200);
+            this.AddBuildingButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AddBuildingButton.Name = "AddBuildingButton";
-            this.AddBuildingButton.Size = new System.Drawing.Size(132, 40);
+            this.AddBuildingButton.Size = new System.Drawing.Size(99, 29);
             this.AddBuildingButton.TabIndex = 2;
             this.AddBuildingButton.Text = "Add";
             this.AddBuildingButton.UseVisualStyleBackColor = true;
@@ -151,24 +160,25 @@ namespace IndividualTask
             // 
             // RemoveBuildingButton
             // 
-            this.RemoveBuildingButton.Location = new System.Drawing.Point(620, 278);
+            this.RemoveBuildingButton.Location = new System.Drawing.Point(465, 200);
+            this.RemoveBuildingButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.RemoveBuildingButton.Name = "RemoveBuildingButton";
-            this.RemoveBuildingButton.Size = new System.Drawing.Size(127, 43);
+            this.RemoveBuildingButton.Size = new System.Drawing.Size(95, 31);
             this.RemoveBuildingButton.TabIndex = 3;
             this.RemoveBuildingButton.Text = "Remove";
             this.RemoveBuildingButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(878, 619);
             this.Controls.Add(this.RemoveBuildingButton);
             this.Controls.Add(this.AddBuildingButton);
             this.Controls.Add(this.BuildingsGroupBox);
             this.Controls.Add(this.BuildingsListBox);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.Text = "Directory of places";
             this.BuildingsGroupBox.ResumeLayout(false);
