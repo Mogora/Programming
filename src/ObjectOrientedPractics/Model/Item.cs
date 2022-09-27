6f59e052cc5a6f2fe81a2ee09f8cafcc1e531181
Тамила Хасанова <tamosca@mail.ru>
2022-09-27 23:25:30 +0300

    27.09.2022 Реализованы классы Item и Customer

---
+++ src/ObjectOrientedPractics/Model/Item.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObjectOrientedPractics.Services;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class Item
+    {
+        /// <summary>
+        /// Уникальный идентификатор для всех объектов данного класса.
+        /// </summary>
+        private readonly int _id;
+
+        /// <summary>
+        /// Название товара.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Описание товара.
+        /// </summary>
+        private string _info;
+
+        /// <summary>
+        /// Стоимость товара.
+        /// </summary>
+        private double _cost;
+
+        /// <summary>
+        /// Все товары.
+        /// </summary>
+        private static int _allItemsCount;
+
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Item"/>
+        /// </summary>
+        public Item()
+        {
+            _allItemsCount++;
+            _id = _allItemsCount;
+        }
+
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Item"/>
+        /// </summary>
+        /// <param name="name">Название товара. Должно быть не более 200 символов.</param>
+        /// <param name="info">Описание товара. Должно быть не более 1000 символов.</param>
+        /// <param name="cost">Стоимость товара. Должна быть в пределах от 0 до 100000.</param>
+        public Item(string name, string info, double cost)
+        {
+            Name = name;
+            Info = info;
+            Cost = cost;
+            _allItemsCount++;
+            _id = _allItemsCount;
+        }
+
+        /// <summary>
+        /// Возвращает уникальный идентификатор товара.
+        /// </summary>
+        public int Id => _id;
+
+        /// <summary>
+        /// Возвращает и задаёт название товара. 
+        /// Должно быть не более 200 символов.
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                ValueValidator.AssertStringOnLength(nameof(Name), 200, value);
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт описание товара. 
+        /// Должно быть не более 1000 символов.
+        /// </summary>
+        public string Info
+        {
+            get => _info;
+            set
+            {
+                ValueValidator.AssertStringOnLength(nameof(Info), 1000, value);
+                _info = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает стоимость товара. 
+        /// Должно быть в пределах от 0 до 100000.
+        /// </summary>
+        public double Cost
+        {
+            get => _cost;
+            set
+            {
+                ValueValidator.AssertValueInRange(nameof(Cost), value, 0, 100000);
+                _cost = value;
+            }
+        }
+    }
+}
---
+++ src/ObjectOrientedPractics/View/Tabs/UserControl1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace ObjectOrientedPractics.View.Tabs
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ItemsListBox = new System.Windows.Forms.ListBox();
+            this.ItemsLabel = new System.Windows.Forms.Label();
+            this.SelectedItemLabel = new System.Windows.Forms.Label();
+            this.IdLabel = new System.Windows.Forms.Label();
+            this.CostLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.DescriptioLabel = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ItemsListBox
+            // 
+            this.ItemsListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ItemsListBox.FormattingEnabled = true;
+            this.ItemsListBox.ItemHeight = 16;
+            this.ItemsListBox.Location = new System.Drawing.Point(3, 29);
+            this.ItemsListBox.Name = "ItemsListBox";
+            this.ItemsListBox.Size = new System.Drawing.Size(499, 708);
+            this.ItemsListBox.TabIndex = 0;
+            // 
+            // ItemsLabel
+            // 
+            this.ItemsLabel.AutoSize = true;
+            this.ItemsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ItemsLabel.Location = new System.Drawing.Point(3, 9);
+            this.ItemsLabel.Name = "ItemsLabel";
+            this.ItemsLabel.Size = new System.Drawing.Size(46, 17);
+            this.ItemsLabel.TabIndex = 1;
+            this.ItemsLabel.Text = "Items";
+            // 
+            // SelectedItemLabel
+            // 
+            this.SelectedItemLabel.AutoSize = true;
+            this.SelectedItemLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SelectedItemLabel.Location = new System.Drawing.Point(3, 9);
+            this.SelectedItemLabel.Name = "SelectedItemLabel";
+            this.SelectedItemLabel.Size = new System.Drawing.Size(106, 17);
+            this.SelectedItemLabel.TabIndex = 2;
+            this.SelectedItemLabel.Text = "Selected Item";
+            // 
+            // IdLabel
+            // 
+            this.IdLabel.AutoSize = true;
+            this.IdLabel.Location = new System.Drawing.Point(3, 45);
+            this.IdLabel.Name = "IdLabel";
+            this.IdLabel.Size = new System.Drawing.Size(25, 17);
+            this.IdLabel.TabIndex = 3;
+            this.IdLabel.Text = "ID:";
+            // 
+            // CostLabel
+            // 
+            this.CostLabel.AutoSize = true;
+            this.CostLabel.Location = new System.Drawing.Point(3, 92);
+            this.CostLabel.Name = "CostLabel";
+            this.CostLabel.Size = new System.Drawing.Size(40, 17);
+            this.CostLabel.TabIndex = 4;
+            this.CostLabel.Text = "Cost:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(53, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.ItemsLabel);
+            this.panel1.Controls.Add(this.ItemsListBox);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(505, 804);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.RemoveButton);
+            this.panel2.Controls.Add(this.AddButton);
+            this.panel2.Controls.Add(this.richTextBox2);
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Controls.Add(this.DescriptioLabel);
+            this.panel2.Controls.Add(this.NameLabel);
+            this.panel2.Controls.Add(this.SelectedItemLabel);
+            this.panel2.Controls.Add(this.IdLabel);
+            this.panel2.Controls.Add(this.CostLabel);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(514, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(692, 804);
+            this.panel2.TabIndex = 8;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(3, 153);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(49, 17);
+            this.NameLabel.TabIndex = 9;
+            this.NameLabel.Text = "Name:";
+            // 
+            // DescriptioLabel
+            // 
+            this.DescriptioLabel.AutoSize = true;
+            this.DescriptioLabel.Location = new System.Drawing.Point(3, 320);
+            this.DescriptioLabel.Name = "DescriptioLabel";
+            this.DescriptioLabel.Size = new System.Drawing.Size(83, 17);
+            this.DescriptioLabel.TabIndex = 10;
+            this.DescriptioLabel.Text = "Description:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 173);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(686, 116);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(3, 340);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(686, 262);
+            this.richTextBox2.TabIndex = 12;
+            this.richTextBox2.Text = "";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(167, 684);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(132, 37);
+            this.AddButton.TabIndex = 13;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(340, 673);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(75, 23);
+            this.RemoveButton.TabIndex = 14;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(3, 743);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(499, 58);
+            this.panel3.TabIndex = 14;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(1209, 817);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ItemsListBox;
+        private System.Windows.Forms.Label ItemsLabel;
+        private System.Windows.Forms.Label SelectedItemLabel;
+        private System.Windows.Forms.Label IdLabel;
+        private System.Windows.Forms.Label CostLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label DescriptioLabel;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
