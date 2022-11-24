8b88fa3980ca900002052acc0b6e3e9d61c95b77
Тамила Хасанова <tamosca@mail.ru>
2022-11-24 19:26:51 +0300

    Реализован класс Address

---
+++ src/ObjectOrientedPractics/Model/Address.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObjectOrientedPractics.Services;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class Address
+    {
+        /// <summary>
+        /// Почтовый индекс.
+        /// </summary>
+        private int _index;
+
+        /// <summary>
+        /// Страна или регион доставки.
+        /// </summary>
+        private string _country;
+
+        /// <summary>
+        /// Город доставки.
+        /// </summary>
+        private string _city;
+
+        /// <summary>
+        /// Улица доставки.
+        /// </summary>
+        private string _street;
+
+        /// <summary>
+        /// Номер дома.
+        /// </summary>
+        private string _building;
+
+        /// <summary>
+        /// Номер квартиры.
+        /// </summary>
+        private string _apartment;
+
+        public Address(int index, string country, string city, string street, string building, string apartment)
+        {
+            Index = index;
+            Country = country;
+            City = city;
+            Street = street;
+            Building = building;
+            Apartment = apartment;
+        }
+
+        public int Index
+        {
+            get
+            {
+                return _index;
+            }
+            set
+            {
+                ValueValidator.AssertSpecifiedNumberCharacters(nameof(Index), value, 6);
+                _index = value;
+            }
+        }
+
+        public string Country
+        {
+            get
+            {
+                return _country;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 50, nameof(Country));
+                _country = value;
+            }
+        }
+
+        public string City
+        {
+            get
+            {
+                return _city;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 50, nameof(City));
+                _city = value;
+            }
+        }
+
+        public string Street
+        {
+            get
+            {
+                return _street;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 100, nameof(Street));
+                _street = value;
+            }
+        }
+
+        public string Building
+        {
+            get
+            {
+                return _building;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 10, nameof(Building));
+                _building = value;
+            }
+        }
+
+        public string Apartment
+        {
+            get
+            {
+                return _apartment;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value, 10, nameof(Apartment));
+            }
+        }
+    }
+}
--- src/ObjectOrientedPractics/Services/ValueValidator.cs
+++ src/ObjectOrientedPractics/Services/ValueValidator.cs
@@ -11,9 +11,9 @@ namespace ObjectOrientedPractics.Services
         /// <summary>
         /// Проверка на то, что строка по длине не больше, чем максимально возможная длина.
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="maxLength"></param>
-        /// <param name="nameProperty"></param>
+        /// <param name="value">Строка.</param>
+        /// <param name="maxLength">Максимальная длина.</param>
+        /// <param name="nameProperty">Имя свойства, откуда был вызван метод.</param>
         public static void AssertStringOnLength(string value, int maxLength, string propertyName)
         {
             if (value.Length > maxLength)
@@ -23,6 +23,13 @@ namespace ObjectOrientedPractics.Services
             }
         }
 
+        /// <summary>
+        /// Проверка на то, что значение находится в нужном диапозоне.
+        /// </summary>
+        /// <param name="nameProperty">Имя свойства, откуда был вызван метод.</param>
+        /// <param name="value">Число</param>
+        /// <param name="minValue">Минимальное значение.</param>
+        /// <param name="maxValue">Максимальное значение.</param>
         public static void AssertValueInRange(string nameProperty, double value, int minValue, int maxValue)
         {
             if (value < minValue || value > maxValue)
@@ -30,6 +37,21 @@ namespace ObjectOrientedPractics.Services
                 throw new ArgumentException ($"Значение {nameProperty} не должно " +
                     $"быть меньше {minValue} и превышать {maxValue} ");
 
+            }
+        }
+
+        /// <summary>
+        /// Проверка на то, что строка имеет указанную длину.
+        /// </summary>
+        /// <param name="nameProperty">Имя свойства, откуда был вызван метод.</param>
+        /// <param name="value">Число.</param>
+        /// <param name="length">Заданная длина.</param>
+        public static void AssertSpecifiedNumberCharacters(string nameProperty, int value, int length)
+        {
+            if(value.ToString().Length != length)
+            {
+                throw new ArgumentException($"Количество символов в строке {nameProperty} должно быть " +
+                    $"равной 6");
             }
         }
     }
