cb248aa3f6c44350348d6283052f5622144e8590
Mogora <67151047+Mogora@users.noreply.github.com>
2023-04-01 20:30:21 +0300

    1.04.2023 Лабораторная №1 Попытка №2

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ObjectOrientedPractics.View.Tabs;
+using ObjectOrientedPractics.Model;
+
+namespace ObjectOrientedPractics
+{
+    public partial class MainForm : Form
+    {
+        private Store _store = new Store();
+
+        /// <summary>
+        /// Предоставляет реализацю расположения элементов на форме.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+            ItemsTab.Items = _store.Items;
+            CustomersTab.Customers = _store.Customers;
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
