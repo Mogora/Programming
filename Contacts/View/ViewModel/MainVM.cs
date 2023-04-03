ad09c414ee4a958b90988bc6d5c987eec2655230
Mogora <67151047+Mogora@users.noreply.github.com>
2023-04-03 23:23:29 +0300

    Add: реализован класс MainVM

---
+++ Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.ComponentModel;
+using View.Model;
+
+namespace View.ViewModel
+{
+    public class MainVM: INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Событие изменения свойства.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Контакт.
+        /// </summary>
+        public Contact Contact { get; private set; } = new Contact();
+
+        /// <summary>
+        /// Возвращает и задаёт путь сериализации. По умолчанию - папка "Мои документы".
+        /// </summary>
+        public string Path { get; set; } =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            + @"\Contacts\contacts.json";
+
+        /// <summary>
+        /// Возвращает и задаёт имя контакта.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return Contact.Name;
+            }
+            set
+            {
+                Contact.Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт электронную почту контакта.
+        /// </summary>
+        public string Email
+        {
+            get
+            {
+                return Contact.Email;
+            }
+            set
+            {
+                Contact.Email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт номер телефона контакта.
+        /// </summary>
+        public string PhoneNumber
+        {
+            get
+            {
+                return Contact.Phone;
+            }
+            set
+            {
+                Contact.Phone = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        /// <summary>
+        /// При вызове зажигает событие <see cref="PropertyChanged"/>.
+        /// </summary>
+        /// <param name="prop">Имя свойства, вызвавшего метод.</param>
+        protected void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
