9d3935d1e1b42151e7efdee7f8c542c65f83c7e0
Mogora <67151047+Mogora@users.noreply.github.com>
2023-05-31 20:22:34 +0300

    31.05.2023 Реализована верстка

--- Contacts/View/Controls/ContactControl.xaml.cs
+++ Contacts/View/Controls/ContactControl.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using View.ViewModel;
 
 namespace View.Controls
 {
@@ -20,9 +11,51 @@ namespace View.Controls
     /// </summary>
     public partial class ContactControl : UserControl
     {
+        /// <summary>
+        /// Регистрирует свойство зависимости для CurrentContact.
+        /// </summary>
+        public static readonly DependencyProperty CurrentContactProperty =
+            DependencyProperty.Register("CurrentContact", typeof(ContactVM),
+            typeof(ContactControl), new UIPropertyMetadata(null));
+
         public ContactControl()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт выбранный контакт.
+        /// </summary>
+        public ContactVM CurrentContact
+        {
+            get => (ContactVM)GetValue(CurrentContactProperty);
+            set => SetValue(CurrentContactProperty, value);
+        }
+
+        private void PhoneNumberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            string pattern = @"[\+\-\(\)\d]";
+            if (!Regex.IsMatch(e.Text, pattern))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void PhoneNumberTextBox_Paste(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string input = (string)e.DataObject.GetData(typeof(string));
+                string pattern = @"[\+\-\(\)\d]";
+                if (!Regex.IsMatch(input, pattern))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
         }
     }
 }
