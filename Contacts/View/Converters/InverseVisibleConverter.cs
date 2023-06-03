53fdcd3c4e1eea6fa0051bc78744e8e48a119b36
Mogora <67151047+Mogora@users.noreply.github.com>
2023-06-04 00:02:13 +0300

    04.06.2023 Добавлен новый конвертер

---
+++ Contacts/View/Converters/InverseVisibleConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Windows.Data;
+
+namespace View.Converters
+{
+    public class InverseVisibleConverter: IValueConverter
+    {
+        /// <summary>
+        /// Конвертирует булевое значение в противоположное.
+        /// </summary>
+        /// <param name="value">Значение.</param>
+        /// <param name="targetType">Целевой тип.</param>
+        /// <param name="parameter">Параметр.</param>
+        /// <param name="culture">Региональная культура</param>
+        /// <returns>Значение видимости элемента.</returns>
+        public object Convert(object value,
+                              Type targetType,
+                              object parameter,
+                              CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+
+            throw new ArgumentException("Value must be bool.");
+        }
+
+        /// <summary>
+        /// Конвертирует булевое значение в противоположное.
+        /// </summary>
+        /// <param name="value">Значение.</param>
+        /// <param name="targetType">Целевой тип.</param>
+        /// <param name="parameter">Параметр.</param>
+        /// <param name="culture">Региональная культура</param>
+        /// <returns>Булевое значение.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+
+            throw new ArgumentException("Value must be bool.");
+        }
+    }
+}
